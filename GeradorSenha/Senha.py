2503d499ab9ad81f98d13c6d68bb0a8d0454149b
Daniel Rogerio Ribeiro <danielzule.dev@gmail.com>
2023-03-01 02:10:54 -0300

    Versão 01 do Gerador de Senha

---
+++ GeradorSenha/Senha.py
@@ -0,0 +1,9 @@
+# Este é um Gerador de senha para criação de Senhas a partir do método de Substituição de Palavra dada pelo Usuário
+# Por Segurança a senha deve possuir 8 a 12 caracteres não especiais
+# A palavra base deverá conter letras Maiuscúlas e Minuscúlas
+# Não utilize Data de Nascimento/Nome/Sobrenome
+
+print ("Olá, Seja Bem Vindo ao Gerador de Senha.")
+chave = input ("Por favor digite uma palavra Chave para a sua senha: ")
+
+print ("Confirme sua Palavra Chave: ", chave)
