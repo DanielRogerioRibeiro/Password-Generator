261e3ccb65dd72cc567e68127a060769aa981a24
Daniel Rogerio Ribeiro <danielzule.dev@gmail.com>
2023-03-04 21:54:54 -0300

    Inclusion of Keyword Validation and possibility to change the keyword

--- GeradorSenha/Senha.py
+++ GeradorSenha/Senha.py
@@ -1,11 +1,36 @@
 # Este é um Gerador de senha para criação de Senhas a partir do método de Substituição de Palavra dada pelo Usuário
 # Por Segurança a senha deve possuir 8 a 12 caracteres não especiais
-# A palavra base deverá conter letras Maiuscúlas e Minuscúlas
+# A palavra base deverá conter letras Maiúsculas e Minúsculas
 # Não utilize Data de Nascimento/Nome/Sobrenome
 
 print ("Olá, Seja Bem Vindo ao Gerador de Senha.")
 chave = input ("Por favor digite uma palavra Chave para a sua senha: ")
 
+print ("Sua Palavra Chave é: ", chave,"?")
+
+#Validação da Senha
+
+val = input ("Digite '1' para Sim ou '2' para não: ")
+validacao = int (val)  
+if validacao == 1:
+    print ("Ok")
+else:
+    chave1 = input ("Por favor digite sua Nova Chave: ")
+    
+    senha1 = ""
+    for letra in chave1:
+        if letra in "a":
+            senha1 = senha1 + "@"
+        elif letra in "E": 
+            senha1 = senha1 + "3"
+        elif letra in "e": 
+            senha1 = senha1 + "&"
+        elif letra in "Ii": 
+            senha1 = senha1 + "!"
+        elif letra in "Oo": 
+            senha1 = senha1 + "0"
+        else: senha1 = senha1 + letra
+  
 # Catálogo de Substituição de Letras
 
 senha = ""
@@ -46,5 +71,12 @@ for letra in chave:
         senha = senha + "11"
     else: senha = senha + letra
 
+
 print ("Nova senha criada com Sucesso!!!!!")
-print ("Sua Nova Senha é: ", senha)
+
+if validacao == 1:
+    print ("Sua Nova Senha é: ",senha)
+else:
+    print ("Sua Nova Senha é: ",senha1)
+
+
