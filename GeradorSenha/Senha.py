a8cfff87ee661b6e02cdddac7fa2be50d6d2db33
Daniel Rogerio Ribeiro <danielzule.dev@gmail.com>
2023-03-05 21:40:53 -0300

    Inserted the Exit System command, correction of the Letter Substitution Catalog that had duplicate information in the Letter S  and matched the Letter Substitution Catalog for both Password and Password1

--- GeradorSenha/Senha.py
+++ GeradorSenha/Senha.py
@@ -1,7 +1,5 @@
-# Este é um Gerador de senha para criação de Senhas a partir do método de Substituição de Palavra dada pelo Usuário
-# Por Segurança a senha deve possuir 8 a 12 caracteres não especiais
-# A palavra base deverá conter letras Maiúsculas e Minúsculas
-# Não utilize Data de Nascimento/Nome/Sobrenome
+# Este é um Gerador de senha para criação de Senhas a partir do método de Substituição de Palavra dada pelo Usuário.
+# Para maiores detalhes acesse README.md 
 
 print ("Olá, Seja Bem Vindo ao Gerador de Senha.")
 chave = input ("Por favor digite uma palavra Chave para a sua senha: ")
@@ -17,21 +15,43 @@ if validacao == 1:
 else:
     chave1 = input ("Por favor digite sua Nova Chave: ")
     
+# Catálogo de Substituição de Letras da Segunda Chave
     senha1 = ""
     for letra in chave1:
         if letra in "a":
             senha1 = senha1 + "@"
-        elif letra in "E": 
+        elif letra in "Ee": 
             senha1 = senha1 + "3"
-        elif letra in "e": 
+        elif letra in "l": 
             senha1 = senha1 + "&"
         elif letra in "Ii": 
             senha1 = senha1 + "!"
         elif letra in "Oo": 
             senha1 = senha1 + "0"
+        elif letra in "Uu": 
+            senha1 = senha1 + "*"
+        elif letra in "Ss": 
+            senha1 = senha1 + "$"
+        elif letra in "Tt": 
+            senha1 = senha1 + "?"
+        elif letra in "Cc": 
+            senha1 = senha1 + "¢"
+        elif letra in "Gg": 
+            senha1 = senha1 + "¨"
+        elif letra in "Bb": 
+            senha1 = senha1 + "5"
+        elif letra in "m": 
+            senha1 = senha1 + "+"
+        elif letra in "n": 
+            senha1 = senha1 + "^"
+        elif letra in "r": 
+            senha1 = senha1 + "~"
+        elif letra in "Ll": 
+            senha1 = senha1 + "11"    
         else: senha1 = senha1 + letra
+
   
-# Catálogo de Substituição de Letras
+# Catálogo de Substituição de Letras da Primeira Chave
 
 senha = ""
 for letra in chave:
@@ -49,10 +69,6 @@ for letra in chave:
         senha = senha + "*"
     elif letra in "Ss": 
         senha = senha + "$"
-    elif letra in "S": 
-        senha = senha + "$"
-    elif letra in "s": 
-        senha = senha + "§"
     elif letra in "Tt": 
         senha = senha + "?"
     elif letra in "Cc": 
@@ -72,11 +88,23 @@ for letra in chave:
     else: senha = senha + letra
 
 
+# Impressão da Nova senha
 print ("Nova senha criada com Sucesso!!!!!")
 
 if validacao == 1:
     print ("Sua Nova Senha é: ",senha)
 else:
     print ("Sua Nova Senha é: ",senha1)
+
+# Finalização do Programa
+while True:
+
+    sair = str( input( 'PRESSIONE "ENTER" PARA SAIR ' ) )
+
+    if not sair:
+
+        exit()
+
+
 
 
